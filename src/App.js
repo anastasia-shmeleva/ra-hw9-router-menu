e4e0df1f392205df74db1de8d9f75dfc306f3d8c
Anastasia <ashmeleva94@gmail.com>
2021-11-28 20:54:46 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Menu from './components/Menu';
+import HomePage from './components/HomePage';
+import DriftPage from './components/DriftPage';
+import TimeAttackPage from './components/TimeAttackPage';
+import ForzaPage from './components/ForzaPage';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Menu />
+        <div className="page">
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/drift" element={<DriftPage/>} />
+            <Route path="/timeattack" element={<TimeAttackPage/>} />
+            <Route path="/forza" element={<ForzaPage/>} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
